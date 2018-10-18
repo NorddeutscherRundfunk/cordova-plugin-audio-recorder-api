c9eb42615a724ee70a374da2bd508870759005f9
Siegmar Mantei <s.mantei@ndr.de>
2018-10-18 09:53:24 +0200

    add: debug info in Log add: Plugin Result PERMISSION_CALL and RECORD_START, in this way the ui know the state

--- src/android/com/emj365/plugins/AudioRecorderAPI.java
+++ src/android/com/emj365/plugins/AudioRecorderAPI.java
@@ -5,11 +5,11 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import android.media.MediaRecorder;
 import android.media.MediaPlayer;
 import android.media.AudioManager;
 import android.os.CountDownTimer;
-import android.os.Environment;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.Manifest;
@@ -22,146 +22,162 @@ import java.io.IOException;
 import java.lang.Throwable;
 
 public class AudioRecorderAPI extends CordovaPlugin {
-  private static final String RECORD = Manifest.permission.RECORD_AUDIO;
-  private static final int AUDIO_RECORD_PERMISSION_CALLBACK = 0;
+    private static final String RECORD = Manifest.permission.RECORD_AUDIO;
+    private static final int AUDIO_RECORD_PERMISSION_CALLBACK = 0;
 
-  private MediaRecorder myRecorder;
-  private String outputFile;
-  private CountDownTimer countDowntimer;
+    private MediaRecorder myRecorder;
+    private String outputFile;
+    private CountDownTimer countDowntimer;
 
-  private CallbackContext callbackContext;
-  private Integer seconds;
+    private CallbackContext callbackContext;
+    private Integer seconds;
 
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    Context context = cordova.getActivity().getApplicationContext();
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        Context context = cordova.getActivity().getApplicationContext();
 
-    if (args.length() >= 1) {
-      seconds = args.getInt(0);
-    } else {
-      seconds = -1;
+        Log.d("AudioRecorderAPI", "-in-> " + action);
+        if (args.length() >= 1) {
+            seconds = args.getInt(0);
+        } else {
+            seconds = -1;
+        }
+        if ("record".equals(action)) {
+            this.callbackContext = callbackContext;
+
+            if (!cordova.hasPermission(RECORD)) {
+                cordova.requestPermission(this, AUDIO_RECORD_PERMISSION_CALLBACK, RECORD);
+                Log.d("AudioRecorderAPI", "AUDIO_RECORD_PERMISSION_CALL");
+                sendPluginResult(PluginResult.Status.OK, "PERMISSION_CALL");
+                return true;
+            } else {
+                return record(context);
+            }
+        }
+
+        if ("stop".equals(action)) {
+            if (countDowntimer != null) {
+                countDowntimer.cancel();
+                countDowntimer = null;
+            }
+            stopRecord(callbackContext);
+            return true;
+        }
+
+        if ("playback".equals(action)) {
+            MediaPlayer mp = new MediaPlayer();
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+            try {
+                FileInputStream fis = new FileInputStream(new File(outputFile));
+                mp.setDataSource(fis.getFD());
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                mp.prepare();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    callbackContext.success("playbackComplete");
+                }
+            });
+            mp.start();
+            return true;
+        }
+
+        return false;
     }
-    if (action.equals("record")) {
-      this.callbackContext = callbackContext;
 
-      if (!cordova.hasPermission(RECORD)) {
-        cordova.requestPermission(this, AUDIO_RECORD_PERMISSION_CALLBACK, RECORD);
+    private void sendPluginResult(PluginResult.Status status, String message) {
+        PluginResult pr = new PluginResult(status, message);
+        pr.setKeepCallback(true);
+        this.callbackContext.sendPluginResult(pr);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                this.callbackContext.error("Permission was denied");
+            }
+
+            switch (requestCode) {
+                case AUDIO_RECORD_PERMISSION_CALLBACK:
+                    this.record(cordova.getActivity().getApplicationContext());
+                    break;
+            }
+        }
+    }
+
+    private boolean record(Context context) {
+        outputFile = context.getFilesDir().getAbsoluteFile() + "/" + UUID.randomUUID().toString() + ".m4a";
+        myRecorder = new MediaRecorder();
+        myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        myRecorder.setAudioSamplingRate(44100);
+        myRecorder.setAudioChannels(1);
+        myRecorder.setAudioEncodingBitRate(44100);
+        myRecorder.setOutputFile(outputFile);
+
+        try {
+            myRecorder.prepare();
+            myRecorder.start();
+            Log.d("AudioRecorderAPI", "record start");
+        } catch (final Exception e) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    Log.d("AudioRecorderAPI", "record", e);
+                    callbackContext.error(e.getMessage());
+                }
+            });
+            return false;
+        }
+        if (seconds != -1) {
+            countDowntimer = new CountDownTimer(seconds * 1000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                }
+
+                public void onFinish() {
+                    stopRecord(callbackContext);
+                }
+            };
+            countDowntimer.start();
+        }
+
+        sendPluginResult(PluginResult.Status.OK, "RECORD_START");
 
         return true;
-      } else {
-        return record(context);
-      }
     }
 
-    if (action.equals("stop")) {
-      if (countDowntimer != null) {
-        countDowntimer.cancel();
-        countDowntimer = null;
-      }
-      stopRecord(callbackContext);
-      return true;
-    }
-
-    if (action.equals("playback")) {
-      MediaPlayer mp = new MediaPlayer();
-      mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-      try {
-        FileInputStream fis = new FileInputStream(new File(outputFile));
-        mp.setDataSource(fis.getFD());
-      } catch (IllegalArgumentException e) {
-        e.printStackTrace();
-      } catch (SecurityException e) {
-        e.printStackTrace();
-      } catch (IllegalStateException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      try {
-        mp.prepare();
-      } catch (IllegalStateException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-        public void onCompletion(MediaPlayer mp) {
-          callbackContext.success("playbackComplete");
+    private void stopRecord(final CallbackContext callbackContext) {
+        try {
+            Log.d("AudioRecorderAPI", "record stop");
+            myRecorder.stop();
+        } catch (Throwable e) {
+            Log.d("AudioRecorderAPI", " stopRecord myRecorder.stop(); " + e.getMessage());
         }
-      });
-      mp.start();
-      return true;
-    }
-
-    return false;
-  }
-
-  public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
-    for (int r : grantResults) {
-      if (r == PackageManager.PERMISSION_DENIED) {
-        this.callbackContext.error("Permission was denied");
-      }
-
-      switch (requestCode) {
-        case AUDIO_RECORD_PERMISSION_CALLBACK:
-          this.record(cordova.getActivity().getApplicationContext());
-          break;
-      }
-    }
-  }
-
-  private boolean record(Context context) {
-    outputFile = context.getFilesDir().getAbsoluteFile() + "/" + UUID.randomUUID().toString() + ".m4a";
-    myRecorder = new MediaRecorder();
-    myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-    myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-    myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-    myRecorder.setAudioSamplingRate(44100);
-    myRecorder.setAudioChannels(1);
-    myRecorder.setAudioEncodingBitRate(44100);
-    myRecorder.setOutputFile(outputFile);
-
-    try {
-      myRecorder.prepare();
-      myRecorder.start();
-    } catch (final Exception e) {
-      cordova.getThreadPool().execute(new Runnable() {
-        public void run() {
-          callbackContext.error(e.getMessage());
+        try {
+            myRecorder.release();
+        } catch (Throwable e) {
+            Log.d("AudioRecorderAPI", " stopRecord myRecorder.release(); " + e.getMessage());
         }
-      });
-      return false;
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                Log.d("AudioRecorderAPI", "<-ou- " + outputFile);
+                callbackContext.success(outputFile);
+            }
+        });
     }
-    if (seconds != -1) {
-      countDowntimer = new CountDownTimer(seconds * 1000, 1000) {
-        public void onTick(long millisUntilFinished) {
-        }
-
-        public void onFinish() {
-          stopRecord(callbackContext);
-        }
-      };
-      countDowntimer.start();
-    }
-    return true;
-  }
-
-  private void stopRecord(final CallbackContext callbackContext) {
-    try {
-      myRecorder.stop();
-    } catch (Throwable e) {
-      Log.d("AudioRecorderAPI", " stopRecord myRecorder.stop(); " + e.getMessage());
-    }
-    try {
-      myRecorder.release();
-    } catch (Throwable e) {
-      Log.d("AudioRecorderAPI", " stopRecord myRecorder.release(); " + e.getMessage());
-    }
-    cordova.getThreadPool().execute(new Runnable() {
-      public void run() {
-        callbackContext.success(outputFile);
-      }
-    });
-  }
 
 }
--- src/ios/AudioRecorderAPI.m
+++ src/ios/AudioRecorderAPI.m
@@ -6,119 +6,148 @@
 #define RECORDINGS_FOLDER [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]
 
 - (void)record:(CDVInvokedUrlCommand*)command {
-  _command = command;
-  if ([_command.arguments count] > 0) {
-    duration = [_command.arguments objectAtIndex:0];
-  }
-  else {
-    duration = nil;
-  }
-
-  [self.commandDelegate runInBackground:^{
-
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-
-    NSError *err;
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
-    if (err)
-    {
-      NSLog(@"%@ %d %@", [err domain], [err code], [[err userInfo] description]);
-    }
-    err = nil;
-    [audioSession setActive:YES error:&err];
-    if (err)
-    {
-      NSLog(@"%@ %d %@", [err domain], [err code], [[err userInfo] description]);
-    }
-
-    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
-    AudioSessionSetProperty (kAudioSessionProperty_OverrideAudioRoute, sizeof (audioRouteOverride),&audioRouteOverride);
-
-    NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] init];
-    [recordSettings setObject:[NSNumber numberWithInt: kAudioFormatMPEG4AAC] forKey: AVFormatIDKey];
-    [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
-    [recordSettings setObject:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
-    [recordSettings setObject:[NSNumber numberWithInt:44100] forKey:AVEncoderBitRateKey];
-    [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-    [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityMedium] forKey: AVEncoderAudioQualityKey];
-
-    // Create a new dated file
-    NSString *uuid = [[NSUUID UUID] UUIDString];
-    recorderFilePath = [NSString stringWithFormat:@"%@/%@.m4a", RECORDINGS_FOLDER, uuid];
-    NSLog(@"recording file path: %@", recorderFilePath);
-
-    NSURL *url = [NSURL fileURLWithPath:recorderFilePath];
-    err = nil;
-    recorder = [[AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:&err];
-    if(!recorder){
-      NSLog(@"recorder: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
-      return;
-    }
-
-    [recorder setDelegate:self];
-
-    if (![recorder prepareToRecord]) {
-      NSLog(@"prepareToRecord failed");
-      return;
-    }
-    if (duration == nil || duration.integerValue == -1) {
-      if (![recorder record]) {
-        NSLog(@"record failed");
-        return;
-      }
+    _command = command;
+    if ([_command.arguments count] > 0) {
+        duration = [_command.arguments objectAtIndex:0];
     }
     else {
-      if (![recorder recordForDuration:(NSTimeInterval)[duration intValue]]) {
-        NSLog(@"recordForDuration failed");
-        return;
-      }
+        duration = nil;
     }
-
-  }];
+    
+    [self.commandDelegate runInBackground:^{
+        
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"PERMISSION_CALL"];
+        [pluginResult setKeepCallbackAsBool:YES];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+        
+        NSError *err = nil;
+        __block NSString *msg = nil;
+        [audioSession requestRecordPermission:^(BOOL granted) {
+            if (granted) {
+                NSLog(@"AudioRecorderAPI Permission granted");
+            }
+            else {
+                msg = @"Permission denied";
+                NSLog(@"AudioRecorderAPI Permission denied");
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Permission was denied"];
+                [pluginResult setKeepCallbackAsBool:NO];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+            }
+        }];
+        
+        if (msg != nil|| msg.length > 0 ){
+            return ;
+        }
+        
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
+        if (err)
+        {
+            NSLog(@"AudioRecorderAPI setCategory %@ %ld %@", [err domain], (long)[err code], [[err userInfo] description]);
+        }
+       
+        
+        err = nil;
+        [audioSession setActive:YES error:&err];
+        if (err)
+        {
+            NSLog(@"AudioRecorderAPI setActive %@ %ld %@", [err domain], (long)[err code], [[err userInfo] description]);
+        }
+   
+        UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
+        AudioSessionSetProperty (kAudioSessionProperty_OverrideAudioRoute, sizeof (audioRouteOverride),&audioRouteOverride);
+        
+        NSMutableDictionary *recordSettings = [[NSMutableDictionary alloc] init];
+        [recordSettings setObject:[NSNumber numberWithInt: kAudioFormatMPEG4AAC] forKey: AVFormatIDKey];
+        [recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey: AVSampleRateKey];
+        [recordSettings setObject:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
+        [recordSettings setObject:[NSNumber numberWithInt:44100] forKey:AVEncoderBitRateKey];
+        [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+        [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityMedium] forKey: AVEncoderAudioQualityKey];
+        
+        // Create a new dated file
+        NSString *uuid = [[NSUUID UUID] UUIDString];
+        recorderFilePath = [NSString stringWithFormat:@"%@/%@.m4a", RECORDINGS_FOLDER, uuid];
+        NSLog(@"AudioRecorderAPI recording file path: %@", recorderFilePath);
+        
+        NSURL *url = [NSURL fileURLWithPath:recorderFilePath];
+        err = nil;
+        recorder = [[AVAudioRecorder alloc] initWithURL:url settings:recordSettings error:&err];
+        if(!recorder){
+            NSLog(@"AudioRecorderAPI recorder: %@ %ld %@", [err domain], (long)[err code], [[err userInfo] description]);
+            return;
+        }
+        
+        [recorder setDelegate:self];
+        
+        if (![recorder prepareToRecord]) {
+            NSLog(@"AudioRecorderAPI prepareToRecord failed");
+            return;
+        }
+        if (duration == nil || duration.integerValue == -1) {
+            if (![recorder record]) {
+                NSLog(@"AudioRecorderAPI record failed");
+                return;
+            } else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"RECORD_START"];
+                [pluginResult setKeepCallbackAsBool:YES];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+                
+            }
+        }
+        else {
+            if (![recorder recordForDuration:(NSTimeInterval)[duration intValue]]) {
+                NSLog(@"AudioRecorderAPI recordForDuration failed");
+                return;
+            }
+        }
+        
+    }];
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
-  _command = command;
-  NSLog(@"stopRecording");
-  [recorder stop];
-  NSLog(@"stopped");
+    _command = command;
+    NSLog(@"AudioRecorderAPI stopRecording");
+    [recorder stop];
+    NSLog(@"AudioRecorderAPI stopped");
 }
 
 - (void)playback:(CDVInvokedUrlCommand*)command {
-  _command = command;
-  [self.commandDelegate runInBackground:^{
-    NSLog(@"recording playback");
-    NSURL *url = [NSURL fileURLWithPath:recorderFilePath];
-    NSError *err;
-    player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
-    player.numberOfLoops = 0;
-    player.delegate = self;
-    [player prepareToPlay];
-    [player play];
-    if (err) {
-      NSLog(@"%@ %d %@", [err domain], [err code], [[err userInfo] description]);
-    }
-    NSLog(@"playing");
-  }];
+    _command = command;
+    [self.commandDelegate runInBackground:^{
+        NSLog(@"AudioRecorderAPI recording playback");
+        NSURL *url = [NSURL fileURLWithPath:recorderFilePath];
+        NSError *err;
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&err];
+        player.numberOfLoops = 0;
+        player.delegate = self;
+        [player prepareToPlay];
+        [player play];
+        if (err) {
+            NSLog(@"AudioRecorderAPI %@ %ld %@", [err domain], (long)[err code], [[err userInfo] description]);
+        }
+        NSLog(@"AudioRecorderAPI playing");
+    }];
 }
 
 - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
-  NSLog(@"audioPlayerDidFinishPlaying");
-  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"playbackComplete"];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+    NSLog(@"AudioRecorderAPI audioPlayerDidFinishPlaying");
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"playbackComplete"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
 }
 
 - (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag {
-  NSURL *url = [NSURL fileURLWithPath: recorderFilePath];
-  NSError *err = nil;
-  NSData *audioData = [NSData dataWithContentsOfFile:[url path] options: 0 error:&err];
-  if(!audioData) {
-    NSLog(@"audio data: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
-  } else {
-    NSLog(@"recording saved: %@", recorderFilePath);
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:recorderFilePath];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
-  }
+    NSURL *url = [NSURL fileURLWithPath: recorderFilePath];
+    NSError *err = nil;
+    NSData *audioData = [NSData dataWithContentsOfFile:[url path] options: 0 error:&err];
+    if(!audioData) {
+        NSLog(@"AudioRecorderAPI audio data: %@ %ld %@", [err domain], (long)[err code], [[err userInfo] description]);
+    } else {
+        NSLog(@"AudioRecorderAPI recording saved: %@", recorderFilePath);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:recorderFilePath];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:_command.callbackId];
+    }
 }
 
 @end
