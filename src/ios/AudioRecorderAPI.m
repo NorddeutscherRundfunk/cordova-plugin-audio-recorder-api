3f280f46f0e76176ebfd841490755cfc05f97a94
Siegmar Mantei <github@smantei.net>
2019-04-25 13:59:41 +0200

    Update AudioRecorderAPI.m
    Set audio quality to high in hope for good record .

--- src/ios/AudioRecorderAPI.m
+++ src/ios/AudioRecorderAPI.m
@@ -64,7 +64,8 @@
         [recordSettings setObject:[NSNumber numberWithInt:1] forKey:AVNumberOfChannelsKey];
         [recordSettings setObject:[NSNumber numberWithInt:44100] forKey:AVEncoderBitRateKey];
         [recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-        [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityMedium] forKey: AVEncoderAudioQualityKey];
+        [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityHigh] forKey: AVEncoderAudioQualityKey];
+        [recordSettings setObject:[NSNumber numberWithInt: AVAudioQualityHigh] forKey: AVEncoderAudioQualityForVBRKey];
         
         // Create a new dated file
         NSString *uuid = [[NSUUID UUID] UUIDString];
